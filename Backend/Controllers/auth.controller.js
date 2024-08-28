4763680f1e1ed691b0fbeb25decfd69f03734934
Huzaifa Zafar <huzaifa@example.com>
2024-08-28 13:29:41 +0500

    git

--- Backend/Controllers/auth.controller.js
+++ Backend/Controllers/auth.controller.js
@@ -1,53 +1,61 @@
 import { userModel } from '../Model/user.model.js'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
+import { v2 as cloudinary } from 'cloudinary';
 
-export const signup = async (req,res,next)=>{
+export const signup = async (req, res, next) => {
     try {
         const user = req.body
-       user.password = await bcrypt.hash(user.password, 10)
-        await userModel.create(user)
-        res.json({
-            success:true,
-            message:"SignUp successfully"
-        })
+        const uploadResult = await cloudinary.uploader.upload(user.avatar, { folder: 'MernCart_Project' })
+            .catch((error) => {
+                next(error)
+            });
+        if (uploadResult) {
+            user.avatar = uploadResult.secure_url
+            user.password = await bcrypt.hash(user.password, 10)
+            await userModel.create(user)
+            res.json({
+                success: true,
+                message: "SignUp successfully"
+            })
+        }
     } catch (error) {
         next(error)
     }
 }
 
-export const login = async (req,res,next)=>{
+export const login = async (req, res, next) => {
     try {
         const user = req.body
 
-        const validUser = await userModel.findOne({username:user.username})        
+        const validUser = await userModel.findOne({ username: user.username })
 
-        if(validUser == null) return next(new Error("Not Valid UserName"))
-        
-        const decodeUser = await bcrypt.compare(user.password,validUser.password)
+        if (validUser == null) return next(new Error("Not Valid UserName"))
 
-        if(!decodeUser) return next(new Error("Incorrect Password"))
+        const decodeUser = await bcrypt.compare(user.password, validUser.password)
+
+        if (!decodeUser) return next(new Error("Incorrect Password"))
 
         const jwt_token = jwt.sign({
-                id:validUser._id,
-                username:validUser.username,
-                role: validUser.roles
-            }, process.env.JWT_SECRET)
-        
+            id: validUser._id,
+            username: validUser.username,
+            role: validUser.roles
+        }, process.env.JWT_SECRET)
+
         try {
-            res.cookie("JWT",jwt_token,{maxAge:900000,httpOnly:true}).json({
-                message:"LogIn successfully",
+            res.cookie("JWT", jwt_token, { maxAge: 900000, httpOnly: true }).json({
+                message: "LogIn successfully",
                 jwt_token
             })
         } catch (error) {
             next(error)
         }
-        
+
     } catch (error) {
         next(error)
     }
 }
 
-export const logout = async (req,res,next)=>{
-    
+export const logout = async (req, res, next) => {
+
 }
